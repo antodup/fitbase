d45e79469efc8b134bb36d09abb2e54f29c7e921
Florian SAUMUROT <florian.saumurot@gmail.com>
2018-02-14 12:29:07 +0100

    modif route connexion/inscription + express session

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const bodyParser =  require('body-parser')
 const mysql =       require('mysql')
 const Twig =        require('twig')
 const bcrypt =      require('bcrypt')
+const session =     require('express-session')
 const app =         express();
 
 /* CREATION DU SERVER */
 const server = require('http').createServer(app);
+var users = null;
 
 /* variable globales */
 var port = 1337;
 
 /* ROAD TO ASSETS DIRECTORY */
+app.use(session())
 app.use('/css', express.static('web-app/assets/css'));
 app.use('/js', express.static('web-app/assets/js'));
 app.use('/img', express.static('web-app/assets/img'));
@@ -56,9 +59,7 @@ app.get('/', function (req, res) {
     res.render('index.twig');
 });
 
-/* road for profil page */
-app.post('/profil', function (req, res) {
-    //Let = à une variable de type var sauf que la la portée change selon son emplacement (limité)
+app.post('/', function (req, res) {
     console.log(req.body.email)
     let q = "select * from users where email like '" + req.body.email + "';",
         co = connection();
@@ -68,8 +69,9 @@ app.post('/profil', function (req, res) {
         if (results.length > 0) {
             bcrypt.compare(req.body.password, results[0].password).then(function (password) {
                 if (password === true) {
-                    req.session = results[0].id;
-                    res.render('profil.twig');
+                    users = results[0].id;
+                    console.log(users)
+                    res.redirect('/profil');
                 } else {
                     console.log('game over')
                     res.render('index.twig', {
@@ -78,20 +80,18 @@ app.post('/profil', function (req, res) {
                 }
             })
         } else {
-            if (Object.keys(req.body).length > 2) {
-                console.log(req.body)
-                var hash = bcrypt.hashSync(req.body.password1, 10);
-                let q = "insert into users (`lastname`, `firstname`, `username`, `birthday`, `email`, `password`, `height`, `weight`, `frequencies`, `objectif`, `profil_picture`, `notification`, `geolocation`) values ('" + req.body.lastname + "', '" + req.body.firstname + "', '" + req.body.username + "', '" + req.body.birthday + "', '" + req.body.email + "', '" + hash + "', " + req.body.height + ", "+ req.body.weight + ", " + req.body.frequencies + ", " + req.body.objectif + ", '', false, false)";
-
-                co.query(q, function (error, results, fields) {
-                    if (error) return console.log(error);
-                    console.log(results.insertId)
-                })
-                res.render("profil.twig");
-            } else {
-                res.redirect('/')
-            }
+            res.render('index.twig')
         }
+    })
+});
+
+/* road for profil page */
+app.get('/profil', function (req, res) {
+    //Let = à une variable de type var sauf que la la portée change selon son emplacement (limité)
+    //récupérer les infos du user en fonction de l'id contenu dans req.session
+    console.log(users)
+    res.render('profil.twig', {
+        id: users
     })
 });
 
@@ -117,11 +117,11 @@ app.get('/inscription', function (req, res) {
         co = connection();
     co.connect();
     co.query("select * from sport;", function (error, results, fields) {
-      sport = results;
-
+        if (error) return console.log(error);
+        sport = results;
         co.query("select * from objectifs;", function (error, results, fields) {
+            if (error) return console.log(error);
             objectif = results;
-
             res.render('register.twig', {
                 sports : sport,
                 objectifs : objectif
@@ -131,6 +131,25 @@ app.get('/inscription', function (req, res) {
 
 
 });
+
+app.post('/inscription', function (req, res) {
+        console.log(req.body)
+        let q = "select * from users where email like '" + req.body.email + "';",
+            co = connection();
+        co.connect();
+        co.query(q, function (error, results, fields) {
+            if (error) return console.log(error);
+            if (results.length > 0) res.redirect('/'); //cet email est deja existant
+            var hash = bcrypt.hashSync(req.body.password1, 10);
+            let q = "insert into users (`lastname`, `firstname`, `username`, `birthday`, `email`, `password`, `height`, `weight`, `frequencies`, `objectif`, `profil_picture`, `notification`, `geolocation`) values ('" + req.body.lastname + "', '" + req.body.firstname + "', '" + req.body.username + "', '" + req.body.birthday + "', '" + req.body.email + "', '" + hash + "', " + req.body.height + ", " + req.body.weight + ", " + req.body.frequencies + ", " + req.body.objectif + ", '', false, false)";
+            co.query(q, function (error, results, fields) {
+                if (error) return console.log(error);
+                req.session = results.insertId;
+                res.redirect("/profil");
+            })
+
+        })
+})
 
 /* road for contact page */
 app.get('/contact', function (req, res) {

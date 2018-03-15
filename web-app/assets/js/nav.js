b1caf0f0224df808b9557d6741711d626ba1a069
antodup <anthony_dupre@live.fr>
2018-03-15 11:38:18 +0100

    nav + register

---
+++ web-app/assets/js/nav.js
@@ -0,0 +1,9 @@
+/*============================================================
+#title            : TITLE
+#description      : Script for 
+#author           : DUPRE ANTHONY
+#date             : XXXX/XX/XX
+#version          : Constantly on progress
+#usage            : JAVASCRIPT
+#notes            : 
+=============================================================*/
